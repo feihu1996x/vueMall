e108ee9616557907521f9f5e15ed725c54105ce0
feihu1996x <gsx0312@qq.com>
2018-10-27 22:28:40 +0800

    实现图片懒加载

--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -10,25 +10,18 @@
             <span class="sortby">Sort by:</span>
             <a href="javascript:void(0)" class="default cur">Default</a>
             <a href="javascript:void(0)" class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
-            <a href="javascript:void(0)" class="filterby stopPop">Filter by</a>
+            <a href="javascript:void(0)" class="filterby stopPop" @click="showFilterPop">Filter by</a>
             </div>
             <div class="accessory-result">
             <!-- filter -->
-            <div class="filter stopPop" id="filter">
+            <div class="filter stopPop" id="filter" v-bind:class="{'filterby-show': filterBy}">
                 <dl class="filter-price">
                 <dt>Price:</dt>
-                <dd><a href="javascript:void(0)">All</a></dd>
                 <dd>
-                    <a href="javascript:void(0)">0 - 100</a>
+                    <a href="javascript:void(0)" v-bind:class="{cur: 'all'==priceChecked}">All</a>
                 </dd>
-                <dd>
-                    <a href="javascript:void(0)">100 - 500</a>
-                </dd>
-                <dd>
-                    <a href="javascript:void(0)">500 - 1000</a>
-                </dd>
-                <dd>
-                    <a href="javascript:void(0)">1000 - 2000</a>
+                <dd v-for="(item,index) in priceFilter" @click="setPriceFilter(index)">
+                    <a href="javascript:void(0)" v-bind:class="{cur: index==priceChecked}">{{item.startPrice}} - {{item.endPrice}}</a>
                 </dd>
                 </dl>
             </div>
@@ -37,60 +30,26 @@
             <div class="accessory-list-wrap">
                 <div class="accessory-list col-4">
                 <ul>
-                    <li>
-                    <div class="pic">
-                        <a href="#"><img src="/static/1.jpg" alt=""></a>
-                    </div>
-                    <div class="main">
-                        <div class="name">XX</div>
-                        <div class="price">999</div>
-                        <div class="btn-area">
-                        <a href="javascript:;" class="btn btn--m">加入购物车</a>
+                    <li v-for="(item, index) in goodsList">
+                        <div class="pic">
+                            <!--<a href="#"><img v-bind:src="item.prodcutImg" alt=""></a>-->
+                            <a href="#"><img v-lazy="item.prodcutImg" alt=""></a>
                         </div>
-                    </div>
-                    </li>
-                    <li>
-                    <div class="pic">
-                        <a href="#"><img src="/static/2.jpg" alt=""></a>
-                    </div>
-                    <div class="main">
-                        <div class="name">XX</div>
-                        <div class="price">1000</div>
-                        <div class="btn-area">
-                        <a href="javascript:;" class="btn btn--m">加入购物车</a>
+                        <div class="main">
+                            <div class="name">{{item.productName}}</div>
+                            <div class="price">{{item.prodcutPrice}}</div>
+                            <div class="btn-area">
+                            <a href="javascript:;" class="btn btn--m">加入购物车</a>
+                            </div>
                         </div>
-                    </div>
-                    </li>
-                    <li>
-                    <div class="pic">
-                        <a href="#"><img src="/static/3.jpg" alt=""></a>
-                    </div>
-                    <div class="main">
-                        <div class="name">XX</div>
-                        <div class="price">500</div>
-                        <div class="btn-area">
-                        <a href="javascript:;" class="btn btn--m">加入购物车</a>
-                        </div>
-                    </div>
-                    </li>
-                    <li>
-                    <div class="pic">
-                        <a href="#"><img src="/static/4.jpg" alt=""></a>
-                    </div>
-                    <div class="main">
-                        <div class="name">XX</div>
-                        <div class="price">2499</div>
-                        <div class="btn-area">
-                        <a href="javascript:;" class="btn btn--m">加入购物车</a>
-                        </div>
-                    </div>
                     </li>
                 </ul>
                 </div>
             </div>
             </div>
         </div>
-        </div>   
+        </div>  
+        <div class="md-overlay" v-show="overLayFlag" @click="closePop"></div>
         <nav-footer></nav-footer>
     </div>
 </template>
@@ -100,29 +59,68 @@
 </style>
 
 <script>
-    /* 
-        引入静态样式
-    */
     import './../assets/css/base.css';
     import './../assets/css/product.css';
     import './../assets/css/login.css';
-
-    /*
-        导入子组件
-    */
     import NavHeader from '@/components/Header';
     import NavFooter from '@/components/Footer';
     import NavBread from '@/components/Bread';
+    import axios from 'axios';
     export default {
         data(){
             return {
-                msg: "商品列表页面"        
+                msg: "商品列表页面",
+                goodsList: [],
+                priceFilter:[
+                    {
+                        startPrice: "0.00",
+                        endPrice: "100.00",
+                    },                    
+                    {
+                        startPrice: "100.00",
+                        endPrice: "500.00",
+                    },
+                    {
+                        startPrice: "500.00",
+                        endPrice: "1000.00",
+                    },             
+                    {
+                        startPrice: "1000.00",
+                        endPrice: "2000.00",
+                    },                                        
+                ],
+                priceChecked: "all",
+                filterBy:false,
+                overLayFlag: false
             }
         },
         components: {
             NavHeader,
             NavFooter,
             NavBread,
+        },
+        mounted(){
+            this.getGoodsList();
+        },
+        methods: {
+            getGoodsList(){
+                axios.get('/goods/list').then((response)=>{
+                    var res = response.data;
+                    this.goodsList = res.result;
+                });
+            },
+            showFilterPop(){
+                this.filterBy = true;
+                this.overLayFlag = true;
+            },
+            closePop(){
+                this.filterBy = false;
+                this.overLayFlag = false;
+            },
+            setPriceFilter(index){
+                this.priceChecked = index;
+                this.closePop();
+            }
         }
     }
 </script>
