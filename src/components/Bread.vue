843b478b1a008b72954aa2a63c33177b4d425aa8
feihu1996x <gsx0312@qq.com>
2018-10-27 20:15:26 +0800

    商品列表组件拆分

---
+++ src/components/Bread.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="nav-breadcrumb-wrap">
+        <div class="container">
+            <nav class="nav-breadcrumb">
+            <a href="/">Home</a>
+            <slot name="bread"></slot>
+            </nav>
+        </div>
+    </div>    
+</template>
+
+<style scope>
+    
+</style>
+
+<script>
+    export default {
+        data(){
+            return {
+                msg: "面包屑组件"
+            }
+        }
+    }
+</script>
