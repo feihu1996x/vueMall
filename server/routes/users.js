18e92aa49f6b8e96dd27a7d15e56bbd9db0c5db8
feihu1996x <gsx0312@qq.com>
2018-10-30 15:42:32 +0800

    登录功能实现

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,48 @@
 var express = require('express');
 var router = express.Router();
+var UserModel = require('./../models/User');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
+});
+
+router.post('/login', (req, res, next) => {
+    let params = {
+        userName: req.body.userName,
+        userPwd: req.body.userPwd,
+    };
+    UserModel.findOne(params, (err, userDoc)=>{
+        if(err){
+            res.json({
+                code: 1,
+                count: 0,
+                msg: err.message,
+                data: []
+            });
+        }else{
+            if(userDoc){
+                res.cookie("userId", userDoc.userId, {
+                    path: "/",
+                    maxAge: 1000*60*60,
+                });
+                // req.session.user = userDoc;
+                res.json({
+                    code: 0,
+                    count: userDoc.length,
+                    msg: "操作成功~",
+                    data: userDoc
+                });
+            }else{
+                res.json({
+                    code: 1,
+                    count: 0,
+                    msg: "用户不存在~",
+                    data: []
+                });
+            }
+        }
+    });
 });
 
 module.exports = router;
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -28,9 +28,9 @@
             <div class="navbar-right-container" style="display: flex;">
             <div class="navbar-menu-container">
                 <!--<a href="/" class="navbar-link">我的账户</a>-->
-                <span class="navbar-link"></span>
-                <a href="javascript:void(0)" class="navbar-link">Login</a>
-                <a href="javascript:void(0)" class="navbar-link">Logout</a>
+                <span class="navbar-link" v-text="nickName" v-if="nickName"></span>
+                <a href="javascript:void(0)" class="navbar-link" @click="loginModalFlag=true" v-if="!nickName">Login</a>
+                <a href="javascript:void(0)" class="navbar-link" v-else>Logout</a>
                 <div class="navbar-cart-container">
                 <span class="navbar-cart-count"></span>
                 <a class="navbar-link navbar-cart-link" href="/#/cart">
@@ -42,6 +42,35 @@
             </div>
             </div>
         </div>
+        <div class="md-modal modal-msg md-modal-transition" v-bind:class="{'md-show':loginModalFlag}">
+          <div class="md-modal-inner">
+            <div class="md-top">
+              <div class="md-title">Login in</div>
+              <button class="md-close" @click="loginModalFlag=false">Close</button>
+            </div>
+            <div class="md-content">
+              <div class="confirm-tips">
+                <div class="error-wrap">
+                  <span class="error error-show" v-show="errorTip">{{errorMsg}}</span>
+                </div>
+                <ul>
+                  <li class="regi_form_input">
+                    <i class="icon IconPeople"></i>
+                    <input type="text" tabindex="1" name="loginname" v-model="userName" class="regi_login_input regi_login_input_left" placeholder="User Name" data-type="loginname">
+                  </li>
+                  <li class="regi_form_input noMargin">
+                    <i class="icon IconPwd"></i>
+                    <input type="password" tabindex="2"  name="password" v-model="userPwd" class="regi_login_input regi_login_input_left login-input-no input_text" placeholder="Password" @keyup.enter="login">
+                  </li>
+                </ul>
+              </div>
+              <div class="login-wrap">
+                <a href="javascript:;" class="btn-login" @click="login">登  录</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="md-overlay" v-if="loginModalFlag" @click="loginModalFlag=false"></div>        
     </header>    
 </template>
 
@@ -118,11 +147,42 @@
 </style>
 
 <script>
-    export default {
-        data(){
-            return {
-                msg: "Header组件"
+import './../assets/css/login.css';
+import axios from 'axios';
+export default {
+    data(){
+        return {
+            msg: "Header组件",
+            userName: 'admin',
+            userPwd: '123456',
+            errorTip: false,
+            loginModalFlag: false,
+            errorMsg: '',
+            nickName: '',
+        }
+    },
+    methods: {
+        login(){
+            if(!this.userName || !this.userPwd){
+                this.errorTip = true;
+                this.errorMsg = '用户名或密码不能为空~';
+                return;
             }
+            axios.post("/users/login",{
+                userName: this.userName,
+                userPwd: this.userPwd
+            }).then((response)=>{
+                let res = response.data;
+                if(0 == res.code){
+                    this.errorTip = false;
+                    this.loginModalFlag = false;
+                    this.nickName = res.data.userName;
+                }else{
+                    this.errorTip = true;
+                    this.errorMsg = res.msg;
+                }
+            });
         }
     }
+}
 </script>
