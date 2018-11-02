3f418dc256b8628c93927b739d55285b0cd20380
feihu1996x <gsx0312@qq.com>
2018-11-02 16:56:40 +0800

    调整静态资源打包路径配置

--- config/index.js
+++ config/index.js
@@ -53,7 +53,8 @@ module.exports = {
         // Paths
         assetsRoot: path.resolve(__dirname, '../dist'),
         assetsSubDirectory: 'static',
-        assetsPublicPath: '/',
+        // assetsPublicPath: '/',
+        assetsPublicPath: '',
 
         /**
          * Source Maps
